648aff528788cdf4c6711205c4c3af5c6ce17e82
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-07-01 13:47:06 +0200

    Update settings.py

--- puppy_store/puppy_store/settings.py
+++ puppy_store/puppy_store/settings.py
@@ -41,6 +41,14 @@ INSTALLED_APPS = [
     'rest_framework'
 ]
 
+# REST Framework
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [],
+    'TEST_REQUEST_DEFAULT_FORMAT': 'json'
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
