19c14c57eba13be679407c48e5cea4c5de393556
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-07-01 15:04:22 +0200

    Update settings.py

--- puppy_store/puppy_store/settings.py
+++ puppy_store/puppy_store/settings.py
@@ -90,6 +90,17 @@ DATABASES = {
     }
 }
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'puppy_store_drf',
+        'USER': 'berkay',
+        'PASSWORD': 'puppy_store',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
