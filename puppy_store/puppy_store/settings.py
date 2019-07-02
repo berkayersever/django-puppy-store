503a23ea8319d672e92b2e7724583db451a87ab6
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-07-02 13:21:27 +0200

    Update settings.py

--- puppy_store/puppy_store/settings.py
+++ puppy_store/puppy_store/settings.py
@@ -82,13 +82,6 @@ WSGI_APPLICATION = 'puppy_store.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-DATABASES = {
-    'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'puppy_store_drf',
         'USER': 'berkay',
